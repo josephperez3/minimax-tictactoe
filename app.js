1f756f22916ceaf12405e8ff6e6c14bb60b55f9e
Joseph Perez <perezj0230@gmail.com>
2021-01-17 03:04:23 -0500

    completed game functionality

--- app.js
+++ app.js
@@ -5,22 +5,43 @@ const cellDivs = document.querySelectorAll(".game-cell");
 let gameIsLive = true;
 let xIsNext = true;
 
-const handleReset = (e) => {
-    console.log(e);
-};
-
 const checkGameStatus = () => {
     gameStatus = [];
     for (const cellDiv of cellDivs) {
         gameStatus.push(cellDiv.classList[2]);
     }
+    if (gameStatus.filter((s) => s == "x").length == 5) {
+        handleTie();
+        return;
+    }
+    // check horizontal win (make fn)
     for (let i = 0; i < 9; i += 3) {
-        if (
-            horizontalWin(gameStatus[i], gameStatus[i + 1], gameStatus[i + 2])
-        ) {
+        if (isWinner(gameStatus[i], gameStatus[i + 1], gameStatus[i + 2])) {
             console.log("row", i / 3);
             handleWinner(gameStatus[i], i, i + 1, i + 2);
+            return;
         }
+    }
+    // check vertical win (make fn)
+    for (let i = 0; i < 3; i++) {
+        if (isWinner(gameStatus[i], gameStatus[i + 3], gameStatus[i + 6])) {
+            console.log("column", i);
+            handleWinner(gameStatus[i], i, i + 3, i + 6);
+            return;
+        }
+    }
+    // check diagonal win (make fn)
+    // left to right diagonal
+    if (isWinner(gameStatus[0], gameStatus[4], gameStatus[8])) {
+        console.log("left to right");
+        handleWinner(gameStatus[0], 0, 4, 8);
+        return;
+    }
+    // right to left diagonal
+    if (isWinner(gameStatus[2], gameStatus[4], gameStatus[6])) {
+        console.log("right to left");
+        handleWinner(gameStatus[2], 2, 4, 6);
+        return;
     }
 };
 
@@ -29,35 +50,60 @@ const handleWinner = (winner, cell1, cell2, cell3) => {
         cellDivs[divIndex].classList.add("winner");
     };
     const changeStatus = () => {
-        // statusDiv.innerHTML = "WINNER: " + winner.toUpperCase();
-        statusDiv.classList.add("winner");
-        console.log(statusDiv);
+        statusDiv.innerHTML = "WINNER: " + winner.toUpperCase();
+        statusDiv.classList.add("winnerText");
     };
     setTimeout(addWinner, 0, cell1);
     setTimeout(addWinner, 175, cell2);
     setTimeout(addWinner, 350, cell3);
     setTimeout(changeStatus, 350);
+    gameIsLive = false;
 };
 
-const horizontalWin = (cell1, cell2, cell3) => {
+const handleTie = () => {
+    statusDiv.innerHTML = "IT'S A TIE!";
+    statusDiv.classList.add("winnerText");
+    gameIsLive = false;
+};
+
+const handleReset = () => {
+    gameIsLive = true;
+    xIsNext = true;
+    for (const cellDiv of cellDivs) {
+        cellDiv.classList.remove("x");
+        cellDiv.classList.remove("o");
+        cellDiv.classList.remove("winner");
+    }
+    statusDiv.classList.remove("winnerText");
+    statusDiv.innerHTML = "X turn";
+};
+
+const isWinner = (cell1, cell2, cell3) => {
     return cell1 && cell1 == cell2 && cell2 == cell3;
 };
 
-const handleCellClick = (e) => {
-    const classList = e.target.classList;
-    const location = e.target.classList[1];
-    if (classList[2] == "x" || classList[2] == "o") {
-        return;
-    }
+const verticalWin = (cell1, cell2, cell3) => {
+    return cell1 && cell1 == cell2;
+};
 
-    if (xIsNext) {
-        classList.add("x");
-        xIsNext = !xIsNext;
-    } else {
-        classList.add("o");
-        xIsNext = !xIsNext;
+const handleCellClick = (e) => {
+    if (gameIsLive) {
+        const classList = e.target.classList;
+        const location = e.target.classList[1];
+        if (classList[2] == "x" || classList[2] == "o") {
+            return;
+        }
+
+        if (xIsNext) {
+            classList.add("x");
+            xIsNext = !xIsNext;
+        } else {
+            classList.add("o");
+            xIsNext = !xIsNext;
+        }
+        statusDiv.innerHTML = (xIsNext ? "X" : "O") + " turn";
+        checkGameStatus();
     }
-    checkGameStatus();
 };
 
 resetDiv.addEventListener("click", handleReset);
